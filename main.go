e1821b7691f67372a8be7671003968777a07d438
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2023-11-22 11:32:10 +0100

    feat(parse): simplify

--- main.go
+++ main.go
@@ -5,31 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 )
-
-type lineCol struct {
-	line, col int
-}
-
-type parsedLine struct {
-	fname      string
-	start, end lineCol
-	statements int
-	covered    int
-}
-
-type cov struct {
-	statements int
-	covered    int
-}
-
-type block struct {
-	start, end lineCol
-}
 
 func main() {
 	file, err := os.Open(os.Args[1])
@@ -40,28 +18,26 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	coverage := make(map[string]map[block]*cov)
-	rx := regexp.MustCompile(`^(.+):(\d+)\.(\d+),(\d+)\.(\d+) (\d+) (\d+)$`)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.HasPrefix(line, "mode:") {
 			continue
 		}
-		p, err := parseLine(rx, line)
+		p, err := parseLine(line)
 		if err != nil {
 			log.Fatal(fmt.Sprintf("failed parsing line '%s': %v", line, err))
 		}
 
-		if _, ok := coverage[p.fname]; !ok {
-			coverage[p.fname] = map[block]*cov{}
+		if _, ok := coverage[p.filename]; !ok {
+			coverage[p.filename] = map[block]*cov{}
 		}
 
-		cov := cov{statements: p.statements, covered: p.covered}
-		b := block{start: p.start, end: p.end}
-		if c, ok := coverage[p.fname][b]; ok {
-			c.covered = max(c.covered, p.covered)
+		cov := cov{numStatements: p.numStatements, hitCount: p.hitCount}
+		if c, ok := coverage[p.filename][p.block]; ok {
+			c.hitCount += p.hitCount
 		} else {
-			coverage[p.fname][b] = &cov
+			coverage[p.filename][p.block] = &cov
 		}
 	}
 
@@ -77,59 +53,23 @@ func main() {
 
 	for _, fn := range fnames {
 		var (
-			statements   int
-			covered      int
-			fileCoverage float64
-			f            = coverage[fn]
+			numStatements int
+			hitCount      int
+			fileCoverage  float64
+			f             = coverage[fn]
 		)
 		for _, cov := range f {
-			statements += cov.statements
-			if cov.covered > 0 {
-				covered += cov.statements
+			numStatements += cov.numStatements
+			if cov.hitCount > 0 {
+				hitCount += cov.numStatements
 			}
 			// covered += cov.covered
 		}
 
-		if statements > 0 {
-			fileCoverage = float64(covered) / float64(statements)
+		if numStatements > 0 {
+			fileCoverage = float64(hitCount) / float64(numStatements)
 		}
 
 		fmt.Printf("%s %f\n", fn, fileCoverage)
 	}
-}
-
-func parseLine(rx *regexp.Regexp, line string) (p parsedLine, err error) {
-	match := rx.FindStringSubmatch(line)
-
-	if len(match) != 8 {
-		return p, fmt.Errorf("unexpected format")
-	}
-
-	p.fname = match[1]
-	p.start.line, err = strconv.Atoi(match[2])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing start line: %w", err)
-	}
-	p.start.col, err = strconv.Atoi(match[3])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing start col: %w", err)
-	}
-	p.end.line, err = strconv.Atoi(match[4])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing end line: %w", err)
-	}
-	p.end.col, err = strconv.Atoi(match[5])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing end col: %w", err)
-	}
-	p.statements, err = strconv.Atoi(match[6])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing numStats: %w", err)
-	}
-	p.covered, err = strconv.Atoi(match[7])
-	if err != nil {
-		return p, fmt.Errorf("failed parsing count: %w", err)
-	}
-
-	return p, nil
 }
