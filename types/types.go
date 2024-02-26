6addad2c08baff8a360758e74648f7b6c4da07cc
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-02-26 21:30:51 +0100

    test(parse): add tests for parsing Go coverage files
    github.com/friendlycaptcha/fcov/app       0.00%
        app.go                                0.00%
        options.go                            0.00%
    github.com/friendlycaptcha/fcov/app/cli   0.00%
        cli.go                                0.00%
        report.go                             0.00%
    github.com/friendlycaptcha/fcov/cmd/fcov  0.00%
        main.go                               0.00%
    github.com/friendlycaptcha/fcov/parse    96.55%
        go.go                                96.55%
    github.com/friendlycaptcha/fcov/report    0.00%
        render.go                             0.00%
        report.go                             0.00%
    github.com/friendlycaptcha/fcov/types    80.00%
        types.go                             80.00%
    
    Total Coverage: 15.06%

--- types/types.go
+++ types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 // FileLocation specifies the line and column number location in a file.
 type FileLocation struct {
 	Line, Col int
@@ -8,6 +10,26 @@ type FileLocation struct {
 // FileBlock specifies the start and end of a segment in a file.
 type FileBlock struct {
 	Start, End FileLocation
+}
+
+// UnmarshalText unmarshals a string in the form of
+// "<startLine>.<startCol>,<endLine>.<endCol>".
+func (fb *FileBlock) UnmarshalText(text []byte) error {
+	var startLine, startCol, endLine, endCol int
+
+	n, err := fmt.Sscanf(string(text), "%d.%d,%d.%d",
+		&startLine, &startCol, &endLine, &endCol)
+	if err != nil {
+		return err
+	}
+	if n != 4 {
+		return fmt.Errorf("wrong file block format: %s", text)
+	}
+
+	fb.Start = FileLocation{Line: startLine, Col: startCol}
+	fb.End = FileLocation{Line: endLine, Col: endCol}
+
+	return nil
 }
 
 // Stats holds coverage related statistics.
